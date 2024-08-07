32295d9530489715252d08ccf2f57923714b9b78
Nozimjon <masteryt123@mail.ru>
2024-08-07 17:10:42 +0500

    1.0.0

---
+++ app.js
@@ -0,0 +1,35 @@
+let x1= document.getElementById('demo1')
+function doWhite1() {
+    if( x1.style.color == 'white' ) { x1.style.color = "black" }
+    else{x1.style.color = 'white'}
+}
+let x2= document.getElementById('demo2')
+function doWhite2() {
+    if( x2.style.color == 'white' ) { x2.style.color = "black" }
+    else{x2.style.color = 'white'}
+}
+let x3= document.getElementById('demo3')
+function doWhite3() {
+    if( x3.style.color == 'white' ) { x3.style.color = "black" }
+    else{x3.style.color = 'white'}
+}
+let x4= document.getElementById('demo4')
+function doWhite4() {
+    if( x4.style.color == 'white' ) { x4.style.color = "black" }
+    else{x4.style.color = 'white'}
+}
+let x5= document.getElementById('demo5')
+function doWhite5() {
+    if( x5.style.color == 'white' ) { x5.style.color = "black" }
+    else{x5.style.color = 'white'}
+}
+let x6= document.getElementById('demo6')
+function doWhite6() {
+    if( x6.style.color == 'white' ) { x6.style.color = "black" }
+    else{x6.style.color = 'white'}
+}
+let x7= document.getElementById('demo7')
+function doWhite7() {
+    if( x7.style.color == 'white' ) { x7.style.color = "black" }
+    else{x7.style.color = 'white'}
+}
